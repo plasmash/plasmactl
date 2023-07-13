df2848f70a66a36ed2f9acea30a25b4b364f34ca
David Ferlay <dferlay@skilld.cloud>
2023-07-13 12:05:36 +0200

    Update

--- get-plasmactl.sh
+++ get-plasmactl.sh
@@ -11,143 +11,147 @@ arch=$(uname -m)
 # Define the URL pattern for the file
 baseurl="https://repositories.skilld.cloud/repository/pla-plasmactl-raw/latest/plasmactl_%s_%s%s"
 
-# Create a log file where every output will be pipe to
+# Create a log file where every output will be piped to
 : "${INSTALL_LOG:=/tmp/get-plasmactl-$(date '+%Y%m%d-%H%M%S').log}"
 pipe=/tmp/get-plasmactl-$$.tmp
-mkfifo $pipe
-tee < $pipe ${INSTALL_LOG} &
+mkfifo "$pipe"
+tee < "$pipe" "${INSTALL_LOG}" &
 exec 1>&-
-exec 1>$pipe 2>&1
-trap 'rm -f $pipe' EXIT
+exec 1>"$pipe" 2>&1
+trap 'rm -f "$pipe"' EXIT
 
-# Avoir unbounded variable
+# Avoid unbounded variable
 has_sudo=""
 footer_notes=""
 
-
 output() {
-    style_start=""
-    style_end=""
-    if [ "${2:-}" != "" ]; then
+  style_start=""
+  style_end=""
+  if [ "${2:-}" != "" ]; then
     case $2 in
-        "success")
-            style_start="\033[0;32m"
-            style_end="\033[0m"
-            ;;
-        "error")
-            style_start="\033[31;31m"
-            style_end="\033[0m"
-            ;;
-        "info"|"warning")
-            style_start="\033[33m"
-            style_end="\033[39m"
-            ;;
-        "heading")
-            style_start="\033[1;33m"
-            style_end="\033[22;39m"
-            ;;
-        "comment")
-            style_start="\033[2m"
-            style_end="\033[22;39m"
-            ;;
+      "success")
+        style_start="$(printf '\033[0;32m')"
+        style_end="$(printf '\033[0m')"
+        ;;
+      "error")
+        style_start="$(printf '\033[31;31m')"
+        style_end="$(printf '\033[0m')"
+        ;;
+      "info" | "warning")
+        style_start="$(printf '\033[33m')"
+        style_end="$(printf '\033[39m')"
+        ;;
+      "heading")
+        style_start="$(printf '\033[1;33m')"
+        style_end="$(printf '\033[22;39m')"
+        ;;
+      "comment")
+        style_start="$(printf '\033[2m')"
+        style_end="$(printf '\033[22;39m')"
+        ;;
     esac
-    fi
+  fi
 
-    builtin echo -e "${style_start}${1}${style_end}"
+  printf "%b%s%b\\n" "${style_start}" "$1" "${style_end}"
 }
 
-
 exit_with_error() {
-    output "Installation failed" "error"
-    output "\nGet help with your plasmactl setup:" "heading"
-    output "- https://im.skilld.cloud/group/pla-plasmactl.client" "heading"
-    output "- https://im.skilld.cloud/group/pla-plasmactl.prod" "heading"
-    output "\nLog file: ${INSTALL_LOG}"
-    output ""
-    exit 1
+  output "Installation failed" "error"
+  output ""
+  output "Get help with your plasmactl setup:" "heading"
+  output "- https://im.skilld.cloud/group/pla-plasmactl.client" "heading"
+  output "- https://im.skilld.cloud/group/pla-plasmactl.prod" "heading"
+  output ""
+  output "Log file: ${INSTALL_LOG}"
+  output ""
+  exit 1
 }
 
 init_sudo() {
-    if [ ! -z "${has_sudo}" ]; then
-        return
-    fi
+  if [ -n "${has_sudo}" ]; then
+    return
+  fi
 
-    has_sudo=false
-    # Are we running the installer as root?
-    if [ "$(echo "$UID")" = "0" ]; then
-        has_sudo=true
-        cmd_sudo=''
+  has_sudo=false
+  # Are we running the installer as root?
+  if [ "$(echo "$UID")" = "0" ]; then
+    has_sudo=true
+    cmd_sudo=''
 
-        return
-    fi
+    return
+  fi
 
-    if command -v sudo > /dev/null 2>&1; then
-        has_sudo=true
-        cmd_sudo='sudo -E'
-    fi
+  if command -v sudo > /dev/null 2>&1; then
+    has_sudo=true
+    cmd_sudo='sudo -E'
+  fi
 }
 
 call_root() {
-    init_sudo
+  init_sudo
 
-    if ! ${has_sudo}; then
-        output "sudo is required to perform this operation" "error"
-        exit_with_error
-    fi
+  if ! "${has_sudo}"; then
+    output "sudo is required to perform this operation" "error"
+    exit_with_error
+  fi
 
-    if $cmd_sudo sh -c "$1" 2>&1 ; then
-        return 0
-    fi
+  if $cmd_sudo sh -c "$1" 2>&1 ; then
+    return 0
+  fi
 
-    return 1
+  return 1
 }
 
 call_try_user() {
-    if ! call_user "$1"; then
-        output "Command failed, re-trying with sudo" "warning"
-        if ! call_root "$1"; then
-            output "${2:-command failed}" "error"
-            exit_with_error
-        fi
+  if ! call_user "$1"; then
+    output "Command failed, re-trying with sudo" "warning"
+    if ! call_root "$1"; then
+      output "${2:-command failed}" "error"
+      exit_with_error
     fi
+  fi
 }
 
 call_user() {
-    sh -c "$1" 2>&1
+  sh -c "$1" 2>&1
 }
 
 add_footer_note() {
-    for var in "$@"; do
-        if [ ! -z "$footer_notes" ]; then
-            footer_notes="${footer_notes}\n${var}"
-        else
-            footer_notes="${footer_notes}${var}"
-        fi
-    done
+  for var in "$@"; do
+    if [ -n "$footer_notes" ]; then
+      footer_notes="${footer_notes}
+${var}"
+    else
+      footer_notes="${footer_notes}${var}"
+    fi
+  done
 }
 
 intro() {
-    output "Starting plasmactl installation..." "success"
+  output "Starting plasmactl installation..." "success"
 }
 
 outro() {
-    output "plasmactl has been installed successfully." "success"
+  output "plasmactl has been installed successfully." "success"
 
-    if command -v "${binaryname}" > /dev/null 2>&1; then
-        output ""
-	"${binaryname}" --version
-    fi
-
-    output "\nWhat's next?" "heading"
-    output "  To use the CLI, run: plasmactl" "output"
-    output "\nUseful links:" "heading"
-    output "  CLI introduction: https://projects.skilld.cloud/skilld/pla-plasmactl/-/blob/master/README.md"
-
-    if [ ! -z "$footer_notes" ]; then
-        output "\nWarning during installation:" "heading"
-        output "$footer_notes" "warning"
-    fi
+  if command -v "${binaryname}" > /dev/null 2>&1; then
     output ""
+    "${binaryname}" --version
+  fi
+
+  output ""
+  output "What's next?" "heading"
+  output "  To use the CLI, run: plasmactl" "output"
+  output ""
+  output "Useful links:" "heading"
+  output "  CLI introduction: https://projects.skilld.cloud/skilld/pla-plasmactl/-/blob/master/README.md"
+
+  if [ -n "$footer_notes" ]; then
+    output ""
+    output "Warning during installation:" "heading"
+    output "$footer_notes" "warning"
+  fi
+  output ""
 }
 
 # Function to validate the credentials and return HTTP status code
@@ -171,28 +175,28 @@ case $os in
     os="darwin"
     extension=""
     ;;
-  CYGWIN*|MINGW32*|MSYS*|MINGW*)
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
     os="windows"
     extension=".exe"
     ;;
   *)
-    echo "Unsupported operating system: $os"
+    output "Unsupported operating system: $os" "error"
     exit 1
     ;;
 esac
 
 case $arch in
-  x86_64|amd64)
+  x86_64 | amd64)
     arch="amd64"
     ;;
-  i?86|x86)
+  i?86 | x86)
     arch="386"
     ;;
-  arm64|aarch64)
+  arm64 | aarch64)
     arch="arm64"
     ;;
   *)
-    echo "Unsupported architecture: $arch"
+    output "Unsupported architecture: $arch" "error"
     exit 1
     ;;
 esac
@@ -203,7 +207,7 @@ intro
 
 # Format the URL with the determined 'os', 'arch' and 'extension' values
 url=$(printf "$baseurl" "$os" "$arch" "$extension")
-echo "Downloading file: ${url}"
+output "Downloading file: ${url}"
 
 # Check if username and password are passed as script arguments
 if [ $# -eq 2 ]; then
@@ -211,20 +215,22 @@ if [ $# -eq 2 ]; then
   password="$2"
 else
   # Prompt user for username and password
-  read -p "Username: " username
-  read -s -p "Password: " password
-  echo
+  output "Username: "
+  read -r username
+  output "Password: "
+  read -r password
+  output ""
 fi
 
 # Check the validity of the credentials
 http_code=$(validate_credentials "$username" "$password" "$url")
 if [ -z "$http_code" ]; then
-  echo "Error: Failed to validate credentials. Access denied."
+  output "Error: Failed to validate credentials. Access denied." "error"
   exit 1
 elif [ "$http_code" -eq 200 ]; then
-  echo "Valid credentials. Access granted."
+  output "Valid credentials. Access granted."
 else
-  echo "Error: HTTP $http_code. Either credentials are invalid or file $binaryname does not exist."
+  output "Error: HTTP $http_code. Either credentials are invalid or file $binaryname does not exist." "error"
   exit 1
 fi
 
@@ -234,7 +240,7 @@ if command -v curl >/dev/null 2>&1; then
 elif command -v wget >/dev/null 2>&1; then
   wget --user="$username" --password="$password" "$url"
 else
-  echo "Neither curl nor wget were found. Please install one of them."
+  output "Neither curl nor wget were found. Please install one of them." "error"
   exit 1
 fi
 
@@ -243,47 +249,48 @@ tempbinaryname=$(basename "$url")
 binaryname="plasmactl${extension}"
 
 if [ ! -e "${tempbinaryname}" ]; then
-  echo "File ${binaryname} does not exist."
+  output "File ${binaryname} does not exist." "error"
   exit 1
 fi
-echo "Renaming file ${tempbinaryname} to ${binaryname}"
-mv ${tempbinaryname} ${binaryname}
+output "Renaming file ${tempbinaryname} to ${binaryname}"
+mv "${tempbinaryname}" "${binaryname}"
 chmod +x "${binaryname}"
 
 # Installing binary
 dirpath="/usr/local/bin"
-if [ -n "${PATH+set}" ] && echo $PATH | grep "/usr/local/binXXX" > /dev/null; then # PATH is defined and includes dir where we can move binary
-  echo "Installing ${binaryname} binary under ${dirpath}"
+if [ -n "${PATH+set}" ] && printf "%s" "$PATH" | grep "/usr/local/bin" > /dev/null; then # PATH is defined and includes dir where we can move binary
+  output "Installing ${binaryname} binary under ${dirpath}"
   #call_try_user "rm -f ${dirpath}/${binaryname}" "Failed to remove ${dirpath}/${binaryname}"
   call_try_user "mv ${binaryname} ${dirpath}" "Failed to move ${binaryname} to ${dirpath}"
 else
-  echo "\$PATH is either undefined, empty or does not contain ${dirpath}"
+  output "\$PATH is either undefined, empty or does not contain ${dirpath}"
   dirpath="$HOME/.plasmactl"
-  if [ ! -d ${dirpath} ]; then mkdir -p ${dirpath} && echo "Creating ${dirpath} directory"; fi
-  echo "Moving ${binaryname} to ${dirpath}"
-  mv ${binaryname} ${dirpath}
+  if [ ! -d "${dirpath}" ]; then mkdir -p "${dirpath}" && output "Creating ${dirpath} directory"; fi
+  output "Moving ${binaryname} to ${dirpath}"
+  mv "${binaryname}" "${dirpath}"
 
-  if ! echo $PATH | grep "${dirpath}" > /dev/null; then
+  if ! printf "%s" "$PATH" | grep "${dirpath}" > /dev/null; then
     output "${dirpath} is not in \$PATH." "warning"
     add_footer_note " ⚠ The directory \"${dirpath}/\" is not in \$PATH"
-    if echo $SHELL | grep '/bin/zsh' > /dev/null; then
+    if printf "%s" "$SHELL" | grep '/bin/zsh' > /dev/null; then
       if ! grep "export PATH=\"${dirpath}:\$PATH\"" "$HOME/.zshrc" > /dev/null; then
         add_footer_note \
-        "   Run this command to add the directory to your PATH:" \
-        "   echo 'export PATH=\"${dirpath}:\$PATH\"' >> \$HOME/.zshrc && source \$HOME/.zshrc"
+          "   Run this command to add the directory to your PATH:" \
+          "   echo 'export PATH=\"${dirpath}:\$PATH\"' >> \$HOME/.zshrc && . \$HOME/.zshrc"
       fi
-    elif echo $SHELL | grep '/bin/bash' > /dev/null; then
+    elif printf "%s" "$SHELL" | grep '/bin/bash' > /dev/null; then
       if ! grep "export PATH=\"${dirpath}:\$PATH\"" "$HOME/.bashrc" > /dev/null; then
         add_footer_note \
-        "   Run this command to add the directory to your PATH:" \
-        "   echo 'export PATH=\"${dirpath}:\$PATH\"' >> \$HOME/.bashrc && source \$HOME/.bashrc"
+          "   Run this command to add the directory to your PATH:" \
+          "   echo 'export PATH=\"${dirpath}:\$PATH\"' >> \$HOME/.bashrc && . \$HOME/.bashrc"
       fi
     else
       add_footer_note \
-      "   You can add it to your PATH by adding this line at the end of your shell configuration file" \
-      "   export PATH=\"${dirpath}:\$PATH\""
+        "   You can add it to your PATH by adding this line at the end of your shell configuration file" \
+        "   export PATH=\"${dirpath}:\$PATH\""
     fi
   fi
 fi
 
 outro
+
