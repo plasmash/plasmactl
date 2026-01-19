f7ef50e0173791909465b4eb07f4abc29202f1aa
Jean-Baptiste Guerraz <861556+jbguerraz@users.noreply.github.com>
2026-01-19 12:30:07 +0000

    chore: update keyring to v0.8.0 and plasmactl-package to v1.1.8
    - keyring v0.8.0 adds Unlock() method for proactive unlock
    - plasmactl-package v1.1.8 improves package:compose output

--- plugins.mk
+++ plugins.mk
@@ -1,11 +1,11 @@
 PLUGIN_LIST := \
   github.com/plasmash/plasmactl/plugin \
   github.com/launchrctl/scaffold@v0.1.1 \
-  github.com/launchrctl/keyring@v0.7.0 \
+  github.com/launchrctl/keyring@v0.8.0 \
   github.com/launchrctl/launchr@v0.22.0 \
   github.com/launchrctl/update@v1.1.2 \
   github.com/launchrctl/web@v0.16.1 \
-  github.com/plasmash/plasmactl-package@v1.1.7 \
+  github.com/plasmash/plasmactl-package@v1.1.8 \
   github.com/plasmash/plasmactl-component@v1.1.1 \
   github.com/plasmash/plasmactl-platform@v1.3.1 \
   github.com/plasmash/plasmactl-processors@v0.1.1
